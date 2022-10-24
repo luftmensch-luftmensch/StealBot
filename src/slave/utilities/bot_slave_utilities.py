9257a63a0c1e1e8052a4668e738cb36ea4503792
francy <@>
2022-10-24 17:52:01 +0200

    Retrevial information

--- src/slave/utilities/bot_slave_utilities.py
+++ src/slave/utilities/bot_slave_utilities.py
@@ -2,6 +2,10 @@
 
 import socket
 from time import sleep
+from cpuinfo import get_cpu_info
+import psutil
+import requests
+import platform
 
 
 def test_connection(hostname: str, port: int) -> bool:
@@ -25,6 +29,36 @@ def test_connection(hostname: str, port: int) -> bool:
             except Exception as e:
                 print(e)
     return result_connection_status
+
+#Retrevial information machine
+def get_processor_name():
+    cpu_info = get_cpu_info() #json with processor information
+    return cpu_info['brand_raw'] #return only cpu name
+
+def get_ram_size():
+    ram_size_byte = psutil.virtual_memory() #get ram size in byte
+    ram_size_gb = (ram_size_byte / 1024**3) #convert byte -> Gb
+    return round(ram_size_gb) #round example: 15.65GB -> 16GB
+
+def get_core_number():
+    logical_core = psutil.cpu_count()
+    physical_core = psutil.cpu_count(logical=False)
+    return str(physical_core), str(logical_core)
+
+def get_hostname():
+    return socket.gethostname()
+
+def get_public_ip():
+    return requests.get('https:/api.ipify.org').text
+
+def get_operating_system():
+    simple_operating_system = platform.uname().system #example: Linux
+    platform_operating_system = platform.platform() #example: Linux-5.15.0-50-generic-x86-64-with-glib2.35
+    return simple_operating_system, platform_operating_system
+
+
+
+
 
 
 if __name__ == "__main__":
