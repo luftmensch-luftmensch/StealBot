25610fa22649b201c0247272191b02afc1e375f3
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-12-04 18:30:46 +0100

    Edits

--- src/slave/main.py
+++ src/slave/main.py
@@ -31,7 +31,6 @@ def start(host: str, port: int, finder: bool):
         current_status = bot_status.get(1)
 
         while True:
-            # TODO: Convertirlo utilizzando il match case -> https://stackoverflow.com/questions/72638083/python-match-case-dictionary-keys
             if current_status == bot_status.get(1):
                 if bot_slave.test_connection(host, port) is True:
                     current_status = bot_status.get(2)
@@ -40,7 +39,7 @@ def start(host: str, port: int, finder: bool):
                     sleep(5)
             elif current_status == bot_status.get(2):
                 loop = asyncio.new_event_loop()
-                loop.run_until_complete(async_client.run_client(host, port))
+                loop.run_until_complete(async_client.run_client(host, port))  # TODO: Modificare in base alla ricerca effettuata dal client
                 current_status = bot_status.get(1)
                 sleep(4)  # TODO: In produzione controllare che le sleep abbiano un tempo sufficiente
 
